b0643f48bcc3c83cf4e771d38c6338bd7adffdbf
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-23 21:37:40 +0300

    created userController

---
+++ server/controllers/userController.js
@@ -0,0 +1,9 @@
+class UserController {
+  async signup(req, res) {}
+
+  async login(req, res) {}
+
+  async auth(req, res) {}
+}
+
+module.exports = new UserController();
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const router = new Router();
+const userController = require("../controllers/userController");
 
-router.post("/signup");
-router.post("/login");
-router.get("/auth");
+router.post("/signup", userController.signup);
+router.post("/login", userController.login);
+router.get("/auth", userController.auth);
 
 module.exports = router;

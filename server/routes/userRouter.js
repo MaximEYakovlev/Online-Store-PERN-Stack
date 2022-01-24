a58aca2eded04c0b5acb74287c22dfd74ca9d689
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-24 23:46:42 +0300

    created middleware: authMiddleware.js

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,10 @@
 const Router = require("express");
 const router = new Router();
 const userController = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
-router.get("/auth", userController.auth);
+router.get("/auth", authMiddleware, userController.auth);
 
 module.exports = router;

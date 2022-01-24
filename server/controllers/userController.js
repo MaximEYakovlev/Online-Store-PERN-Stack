f07357192b7fc0faf3c410c15461bf1670cdba6c
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-24 23:06:51 +0300

    installed & used JSON Web Token

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,28 @@
 const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models");
 
 class UserController {
-  async signup(req, res) {}
+  async signup(req, res, next) {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest("Invalid email or password."));
+    }
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(ApiError.badRequest("User with this email already exists"));
+    }
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const token = jwt.sign(
+      { id: user.id, email, role },
+      process.env.SECRET_KEY,
+      { expiresIn: "24h" }
+    );
+    return res.json({ token });
+  }
 
   async login(req, res) {}
 
